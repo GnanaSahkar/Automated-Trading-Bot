2140448abb0149f9aeda68b2aaec82a8a3057683
GnanaSahkar <gnanasahkar123@gmail.com>
2024-06-04 08:28:04 +0530

    main.py

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ from utils.data_fetcher import fetch_historical_data
 from stratergies.moving_average import MovingAverageStrategy
 
 def main():
-    # Fetch historical data
     data_path = os.path.join('data', 'historical', 'historical_data.csv')
     data = fetch_historical_data(data_path)
 
-    # Initialize strategy
     strategy = MovingAverageStrategy(short_window=40, long_window=100)
-
-    # Generate signals
     signals = strategy.generate_signals(data)
 
-    # Print the generated signals
+
     print(signals)
 
 if __name__ == "__main__":
